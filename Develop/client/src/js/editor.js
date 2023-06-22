247f7712687310eef6f435e3cf4ba82515b7f8d6
Dallen Hatton <Dhatton2001@gmail.com>
2023-06-21 20:40:29 -0400

    edit + install

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -1,42 +1,41 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
-import { header } from './header';
+import { openDB } from 'idb';
 
-export default class {
-  constructor() {
-    const localData = localStorage.getItem('content');
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
 
-    // check if CodeMirror is loaded
-    if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
-    }
-
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
-      lineNumbers: true,
-      lineWrapping: true,
-      autofocus: true,
-      indentUnit: 2,
-      tabSize: 2,
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+    console.log("Put to the DB");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({
+      id: 1,
+      text: content
     });
+    const result = await request;
+    console.log("🚀 - data saved to the database", result);
+};
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
-    });
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+    console.log("GET all from the database");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getAll();
+    const result = await request;
+    console.log("result.value", result);
+    return result.value;
+};
 
-    this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
-    });
-
-    // Save the content of the editor when the editor itself is loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
-    });
-  }
-}
+initdb();
